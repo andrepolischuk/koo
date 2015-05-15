b15078c5e1ec137c2ba2c643949972759e1221e7
andrepolischuk <andre.polischuk@gmail.com>
2015-05-15 12:15:41 +0300

    Fix eslint errors

--- index.js
+++ index.js
@@ -1,142 +1,65 @@
-// Koo © 2014-2015 Andrey Polischuk
-// github.com/andrepolischuk/koo
 
-!function() {
+'use strict';
 
-  'use strict';
+/**
+ * Expose get cookie
+ *
+ * @param  {String} name
+ * @return {String|Object}
+ * @api public
+ */
 
-  /**
-   * Get document cookies
-   * @return {Object}
-   * @api private
-   */
+module.exports.get = function(name) {
+  if (!document.cookie || !document.cookie.length) return;
+  var split = document.cookie.split(';');
+  var cookies = {};
 
-  function getCookies() {
-
-    var result = {};
-
-    if (document.cookie && document.cookie !== '') {
-
-      var cookies = document.cookie.split(';');
-
-      for (var i = 0, cookie; i < cookies.length; i++) {
-
-        // define item
-        cookie = cookies[i].replace(/^(\s|\u00A0)+/g, '').split('=');
-
-        // item to result object
-        result[cookie[0]] = decodeURIComponent(cookie[1]);
-
-      }
-
-    }
-
-    return result;
-
+  for (var i = 0, item; i < split.length; i++) {
+    item = split[i].replace(/^(\s|\u00A0)+/g, '').split('=');
+    cookies[item[0]] = decodeURIComponent(item[1]);
   }
 
-  /**
-   * Set document cookie
-   * @param {String} name
-   * @param {String} value
-   * @param {Object} options
-   * @api private
-   */
+  return name ? cookies[name] : cookies;
+};
 
-  function setCookie(name, value, options) {
+/**
+ * Expose set cookie
+ *
+ * @param  {String} name
+ * @param  {String} value
+ * @param  {Object} options
+ * @return {String}
+ * @api public
+ */
 
-    if (name !== undefined && value !== undefined) {
+module.exports.set = function(name, value, options) {
+  if (name === undefined || value === undefined) return;
+  options = options || {};
+  var date = options.expires || {};
 
-      options = options || {};
-
-      var expires = '';
-
-      if (options.expires && (typeof options.expires === 'number' || options.expires.toUTCString)) {
-
-        var date;
-
-        if (typeof options.expires === 'number') {
-          date = new Date();
-          date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-        } else {
-          date = options.expires;
-        }
-
-        expires = '; expires=' + date.toUTCString();
-
-      }
-
-      // define cookie params
-      var path   = options.path ? '; path=' + (options.path) : '';
-      var domain = options.domain ? '; domain=' + (options.domain) : '';
-      var secure = options.secure ? '; secure' : '';
-
-      document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-
-    }
-
+  if (typeof options.expires === 'number') {
+    date = new Date();
+    date.setTime(date.getTime() + options.expires * 24 * 60 * 60 * 1000);
   }
 
-  /**
-   * Initialize object
-   * @api public
-   */
+  var cookie = name + '=' + encodeURIComponent(value);
+  cookie += date.toUTCString ? '; expires=' + date.toUTCString() : '';
+  cookie += options.path ? '; path=' + options.path : '';
+  cookie += options.domain ? '; domain=' + options.domain : '';
+  cookie += options.secure ? '; secure' : '';
 
-  function koo() {
+  document.cookie = cookie;
+  return cookie;
+};
 
-  }
+/**
+ * Expose remove cookie
+ *
+ * @param  {String} name
+ * @return {String}
+ * @api public
+ */
 
-  /**
-   * Get cookie
-   * @param {String} name
-   * @api public
-   */
-
-  koo.get = function(name) {
-    var cookies = getCookies();
-    return name ? cookies[name] : cookies;
-  };
-
-  /**
-   * Set cookie
-   * @param {String} name
-   * @param {String} value
-   * @param {Object} options
-   * @api public
-   */
-
-  koo.set = function(name, value, options) {
-    setCookie(name, value, options);
-  };
-
-  /**
-   * Remove cookie
-   * @param {String} name
-   * @api public
-   */
-
-  koo.remove = function(name) {
-    setCookie(name, '', { 'expires' : -1 });
-  };
-
-  /**
-   * Module exports
-   */
-
-  if (typeof define === 'function' && define.amd) {
-
-    define([], function() {
-      return koo;
-    });
-
-  } else if (typeof module !== 'undefined' && module.exports) {
-
-    module.exports = koo;
-
-  } else {
-
-    this.koo = koo;
-
-  }
-
-}.call(this);
+module.exports.remove = function(name) {
+  return module.exports.set(name, '', {expires: -1});
+};
